e8816964fdcfaf711de73f43e769ad489359658f
Finanflix <projectfinanflix@gmail.com>
2025-04-28 18:40:03 -0300

    refact

--- index.js
+++ index.js
@@ -1,139 +1,231 @@
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-// const io = require("socket.io")(http);
-const PORT = process.env.PORT || 3001;
-const path = require("path");
+// const express = require("express");
+// const app = express();
+// const http = require("http").createServer(app);
+// // const io = require("socket.io")(http);
+// const PORT = process.env.PORT || 3001;
+// const path = require("path");
 
-const io = require("socket.io")(http, {
-  transports: ["websocket", "polling"],
-  cors: {
-    origin: "*", // Reemplaza con tu URL de frontend
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true, // Si necesitas compartir cookies u otras credenciales
-  },
-});
+// // const io = require("socket.io")(http, {
+// //   transports: ["websocket", "polling"],
+// //   cors: {
+// //     origin: "*", // Reemplaza con tu URL de frontend
+// //     methods: ["GET", "POST"],
+// //     allowedHeaders: ["Content-Type"],
+// //     credentials: true, // Si necesitas compartir cookies u otras credenciales
+// //   },
+// // });
 
 // const { Server } = require("socket.io");
 
 // const io = new Server({
 //   cors: {
-//     // origin: "http://localhost:5173",
-//     origin: "*",
+//     origin: "https://client-videocall.vercel.app", // Asegúrate de usar la URL de tu frontend desplegado en Vercel
 //     methods: ["GET", "POST"],
 //   },
 // });
 
-let socketList = {};
+// let socketList = {};
 
-// app.use(express.static(path.join(__dirname, 'public'))); // this will work for CRA build
-app.use(express.static(path.join(__dirname, "../vite")));
+// // app.use(express.static(path.join(__dirname, 'public'))); // this will work for CRA build
+// app.use(express.static(path.join(__dirname, "../vite")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-}
+//   app.get("/*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
+//   });
+// }
 
-// Route
-app.get("/ping", (req, res) => {
-  res
-    .send({
-      success: true,
-    })
-    .status(200);
-});
+// // Route
+// app.get("/ping", (req, res) => {
+//   res
+//     .send({
+//       success: true,
+//     })
+//     .status(200);
+// });
 
-// Socket
-io.on("connection", (socket) => {
-  console.log(`New User connected: ${socket.id}`);
+// // Socket
+// io.on("connection", (socket) => {
+//   console.log(`New User connected: ${socket.id}`);
 
-  socket.on("disconnect", () => {
-    socket.disconnect();
-    console.log("User disconnected!");
-  });
+//   socket.on("disconnect", () => {
+//     socket.disconnect();
+//     console.log("User disconnected!");
+//   });
 
-  socket.on("BE-check-user", ({ roomId, userName }) => {
-    let error = false;
-    console.log("BE-check-user", roomId, userName);
-    io.sockets.in(roomId).clients((err, clients) => {
-      clients.forEach((client) => {
-        if (socketList[client] == userName) {
-          error = true;
-        }
+//   socket.on("BE-check-user", ({ roomId, userName }) => {
+//     let error = false;
+//     console.log("BE-check-user", roomId, userName);
+//     io.sockets.in(roomId).clients((err, clients) => {
+//       clients.forEach((client) => {
+//         if (socketList[client] == userName) {
+//           error = true;
+//         }
+//       });
+//       socket.emit("FE-error-user-exist", { error });
+//     });
+//   });
+
+//   /**
+//    * Join Room
+//    */
+//   socket.on("BE-join-room", ({ roomId, userName }) => {
+//     // Socket Join RoomName
+//     console.log("BE-join-room", roomId, userName);
+//     socket.join(roomId);
+//     socketList[socket.id] = { userName, video: true, audio: true };
+
+//     // Set User List
+//     io.sockets.in(roomId).clients((err, clients) => {
+//       try {
+//         const users = [];
+//         clients.forEach((client) => {
+//           // Add User List
+//           users.push({ userId: client, info: socketList[client] });
+//         });
+//         socket.broadcast.to(roomId).emit("FE-user-join", users);
+//         // io.sockets.in(roomId).emit('FE-user-join', users);
+//       } catch (e) {
+//         io.sockets.in(roomId).emit("FE-error-user-exist", { err: true });
+//       }
+//     });
+//   });
+
+//   socket.on("BE-call-user", ({ userToCall, from, signal }) => {
+//     io.to(userToCall).emit("FE-receive-call", {
+//       signal,
+//       from,
+//       info: socketList[socket.id],
+//     });
+//   });
+
+//   socket.on("BE-accept-call", ({ signal, to }) => {
+//     io.to(to).emit("FE-call-accepted", {
+//       signal,
+//       answerId: socket.id,
+//     });
+//   });
+
+//   socket.on("BE-send-message", ({ roomId, msg, sender }) => {
+//     io.sockets.in(roomId).emit("FE-receive-message", { msg, sender });
+//   });
+
+//   socket.on("BE-leave-room", ({ roomId, leaver }) => {
+//     delete socketList[socket.id];
+//     socket.broadcast
+//       .to(roomId)
+//       .emit("FE-user-leave", { userId: socket.id, userName: [socket.id] });
+//     io.sockets.sockets[socket.id].leave(roomId);
+//   });
+
+//   socket.on("BE-toggle-camera-audio", ({ roomId, switchTarget }) => {
+//     if (switchTarget === "video") {
+//       socketList[socket.id].video = !socketList[socket.id].video;
+//     } else {
+//       socketList[socket.id].audio = !socketList[socket.id].audio;
+//     }
+//     socket.broadcast
+//       .to(roomId)
+//       .emit("FE-toggle-camera", { userId: socket.id, switchTarget });
+//   });
+// });
+// console.log(socketList);
+
+// http.listen(PORT, () => {
+//   console.log("Connected : 3001");
+// });
+
+// api/socket.js
+import { Server } from "socket.io";
+
+// Reemplaza la URL con la URL de tu servidor WebSocket real (Heroku, AWS, etc)
+const ioUrl = "https://server-socket-videocall.vercel.app";
+
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    // Este endpoint podría ser usado para verificar la conexión
+    return res.status(200).json({ success: true });
+  } else if (req.method === "POST") {
+    // Establecer la conexión a un servidor WebSocket externo (ioUrl)
+    const io = new Server(ioUrl, {
+      cors: {
+        origin: "https://client-videocall.vercel.app", // La URL de tu frontend
+        methods: ["GET", "POST"],
+      },
+    });
+
+    const socket = io.of("/");
+
+    socket.on("connection", (socket) => {
+      console.log(`New User connected: ${socket.id}`);
+
+      socket.on("disconnect", () => {
+        socket.disconnect();
+        console.log("User disconnected!");
       });
-      socket.emit("FE-error-user-exist", { error });
-    });
-  });
 
-  /**
-   * Join Room
-   */
-  socket.on("BE-join-room", ({ roomId, userName }) => {
-    // Socket Join RoomName
-    console.log("BE-join-room", roomId, userName);
-    socket.join(roomId);
-    socketList[socket.id] = { userName, video: true, audio: true };
-
-    // Set User List
-    io.sockets.in(roomId).clients((err, clients) => {
-      try {
-        const users = [];
-        clients.forEach((client) => {
-          // Add User List
-          users.push({ userId: client, info: socketList[client] });
+      socket.on("BE-check-user", ({ roomId, userName }) => {
+        let error = false;
+        console.log("BE-check-user", roomId, userName);
+        socket.broadcast.to(roomId).clients((err, clients) => {
+          clients.forEach((client) => {
+            if (socketList[client] == userName) {
+              error = true;
+            }
+          });
+          socket.emit("FE-error-user-exist", { error });
         });
+      });
+
+      socket.on("BE-join-room", ({ roomId, userName }) => {
+        console.log("BE-join-room", roomId, userName);
+        socket.join(roomId);
+        socketList[socket.id] = { userName, video: true, audio: true };
+
         socket.broadcast.to(roomId).emit("FE-user-join", users);
-        // io.sockets.in(roomId).emit('FE-user-join', users);
-      } catch (e) {
-        io.sockets.in(roomId).emit("FE-error-user-exist", { err: true });
-      }
+      });
+
+      socket.on("BE-call-user", ({ userToCall, from, signal }) => {
+        io.to(userToCall).emit("FE-receive-call", {
+          signal,
+          from,
+          info: socketList[socket.id],
+        });
+      });
+
+      socket.on("BE-accept-call", ({ signal, to }) => {
+        io.to(to).emit("FE-call-accepted", {
+          signal,
+          answerId: socket.id,
+        });
+      });
+
+      socket.on("BE-send-message", ({ roomId, msg, sender }) => {
+        io.sockets.in(roomId).emit("FE-receive-message", { msg, sender });
+      });
+
+      socket.on("BE-leave-room", ({ roomId, leaver }) => {
+        delete socketList[socket.id];
+        socket.broadcast
+          .to(roomId)
+          .emit("FE-user-leave", { userId: socket.id, userName: [socket.id] });
+        socket.leave(roomId);
+      });
+
+      socket.on("BE-toggle-camera-audio", ({ roomId, switchTarget }) => {
+        if (switchTarget === "video") {
+          socketList[socket.id].video = !socketList[socket.id].video;
+        } else {
+          socketList[socket.id].audio = !socketList[socket.id].audio;
+        }
+        socket.broadcast
+          .to(roomId)
+          .emit("FE-toggle-camera", { userId: socket.id, switchTarget });
+      });
     });
-  });
 
-  socket.on("BE-call-user", ({ userToCall, from, signal }) => {
-    io.to(userToCall).emit("FE-receive-call", {
-      signal,
-      from,
-      info: socketList[socket.id],
-    });
-  });
-
-  socket.on("BE-accept-call", ({ signal, to }) => {
-    io.to(to).emit("FE-call-accepted", {
-      signal,
-      answerId: socket.id,
-    });
-  });
-
-  socket.on("BE-send-message", ({ roomId, msg, sender }) => {
-    io.sockets.in(roomId).emit("FE-receive-message", { msg, sender });
-  });
-
-  socket.on("BE-leave-room", ({ roomId, leaver }) => {
-    delete socketList[socket.id];
-    socket.broadcast
-      .to(roomId)
-      .emit("FE-user-leave", { userId: socket.id, userName: [socket.id] });
-    io.sockets.sockets[socket.id].leave(roomId);
-  });
-
-  socket.on("BE-toggle-camera-audio", ({ roomId, switchTarget }) => {
-    if (switchTarget === "video") {
-      socketList[socket.id].video = !socketList[socket.id].video;
-    } else {
-      socketList[socket.id].audio = !socketList[socket.id].audio;
-    }
-    socket.broadcast
-      .to(roomId)
-      .emit("FE-toggle-camera", { userId: socket.id, switchTarget });
-  });
-});
-console.log(socketList);
-
-http.listen(PORT, () => {
-  console.log("Connected : 3001");
-});
+    res.status(200).json({ success: true });
+  }
+}
